443b1738b5440d1e1f1eee7bd0b7b9a454bc2f2d
vgilko <v.gilko@mkskom.com>
2022-12-16 17:22:04 +0300

    ci

--- lab_data/postgres/20-create-databases.sh
+++ lab_data/postgres/20-create-databases.sh
@@ -3,8 +3,6 @@ set -e
 
 # TODO для создания баз прописать свой вариант
 export VARIANT="v3"
-export SCRIPT_PATH=./lab_data/postgres/docker-entrypoint-initdb.d/
+export SCRIPT_PATH=/docker-entrypoint-initdb.d/
 export PGPASSWORD=postgres
-echo $SCRIPT_PATH
-psql -f "$SCRIPT_PATH/scripts/db-v3.sql"
-psql -f "$SCRIPT_PATH/scripts/cars.sql"
+psql -f "$SCRIPT_PATH/scripts/db-$VARIANT.sql"
